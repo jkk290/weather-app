1fd2ffb947e81b50ba2a6651ebb8ee86f9ab41f0
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-05-22 14:21:28 -1000

    Add function to fetch then output weather info

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import "./styles.css";
 
-alert("Hello World");
+function getWeather(location) {
+        fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/today?unitGroup=us&key=WEWSPHRRZDT9YMZW3V9T6MB8M&contentType=json`, {
+        "method": "GET",
+        "headers": {
+         }
+    })
+    .catch(err => {
+    console.error(err);
+    })
+    .then(response => {
+    return response.json();
+    })
+    .then(data => displayWeatherInfo(data));
+};
+
+function displayWeatherInfo(data) {
+    const description = data.description;
+    const conditions = data.currentConditions.conditions;
+    const temp = data.currentConditions.temp;
+    const humidity = data.currentConditions.humidity;
+    const feelsLike = data.currentConditions.feelslike;
+    const uvIndex = data.currentConditions.uvindex;
+
+    console.log(`Current weather: ${conditions} \nTemperature (F): ${temp} \nHumidity: ${humidity} \nFeels Like (F): ${feelsLike} \nUV Index: ${uvIndex} \n${description}`);
+
+};
+
+getWeather('Honolulu');
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let location = document.getElementById('city').value;
+    getWeather(location);
+});
+
