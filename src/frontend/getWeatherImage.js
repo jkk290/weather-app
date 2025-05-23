f213a6b85f40065e2d93ec8e1b6bbe4e47cb2ea9
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-05-23 11:50:32 -1000

    Refactor weatherImage to an async/await function

--- src/frontend/getWeatherImage.js
+++ src/frontend/getWeatherImage.js
@@ -1,12 +1,19 @@
-export function weatherImage(weather) {
-
-    const img = document.querySelector('img');
+export async function weatherImage(weather) {
+    try {
+        const img = document.querySelector('img');
     
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=tqRGGU94sIlpObLiNLNBxEWAnlnG65Gv&s=${weather}`, {mode: 'cors'})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        img.src = response.data.images.original.url;
-    });
+        const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=tqRGGU94sIlpObLiNLNBxEWAnlnG65Gv&s=${weather}`, {mode: 'cors'});
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! ${response.status}`);            
+        }
+
+        const result = await response.json();
+        const loadImage = result.data.images.original.url;
+        img.src = loadImage;
+        
+    } catch(error) {
+        console.log('Error fetching gif', error);
+    }
+    
 };
